17b06495bf59d9c34738d196077cb54740151acb
mariod93 <200403@student.pwr.wroc.pl>
2014-03-11 11:35:05 +0100

    wersja 1.0

---
+++ src/klasa.cpp
@@ -0,0 +1,10 @@
+/*
+ * klasa.cpp
+ *
+ *  Created on: 11 mar 2014
+ *      Author: Mario
+ */
+
+
+
+
---
+++ src/klasa.hh
@@ -0,0 +1,15 @@
+/*
+ * klasa.hh
+ *
+ *  Created on: 11 mar 2014
+ *      Author: Mario
+ */
+
+#ifndef KLASA_HH_
+#define KLASA_HH_
+
+
+
+
+
+#endif /* KLASA_HH_ */
